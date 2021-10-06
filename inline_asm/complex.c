ca14057aaa82a5405ff1ac97b571c27d6ebf2c31
Curt Bridgers <bridgerspc@gmail.com>
2021-10-05 22:10:01 -0400

    Added example of adding two variables to an output variable with extended ascii, no function call

---
+++ inline_asm/complex.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int sum(int val_1, int val_2);
+
+int main(int argc, char *argv[])
+{
+    int operand1 = 20;
+    int operand2 = 20;
+    int output;
+
+    asm volatile (
+        "movl %1, %0;"           // Move operand1 to output
+        "addl %2, %0;"           // Add operand2 to output
+        : "=rm" (output)
+        : "irm" (operand1), "irm" (operand2) );
+
+
+    printf("%d + %d = %d\n", operand1, operand2, output);
+    return EXIT_SUCCESS;
+}
+
+
