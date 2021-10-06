5c68d90dc6dbb9124163d41bea701280ca81b2b6
Curt Bridgers <bridgerspc@gmail.com>
2021-10-06 09:19:24 -0400

    Added macros folder and a macro version of /inline_asm/ret_addr.c

---
+++ macros/ret_addr.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <inttypes.h>
+
+// This is the same return address printing function
+// as is found is ../inline_asm/ret_addr.c but in macro
+// form.
+#define ret_addr() { \
+    uint64_t addr; \
+    asm volatile ( \
+        "mov $1, %%rax;" \
+        "mov (%%rbp, %%rax, 8), %0;" \
+        : "=rm" (addr) ); \
+    printf("Ret Addr: 0x%" PRIx64 "\n", addr); \
+}
+
+int foo() {
+    int operand1 = 30;
+    int operand2 = 25;
+
+    ret_addr();
+
+    return operand1 + operand2;
+}
+
+int main(int argc, char *argv[])
+{
+    printf("30 + 25 = %d\n", foo());
+	return EXIT_SUCCESS;	
+}
+
