970ee23c9c997c309a4e223f246383dc2bc9f1e8
Curt Bridgers <bridgerspc@gmail.com>
2021-10-14 00:59:37 -0400

    Updated comments, brought init and cleanup code into the main function

--- nCurses/panels.c
+++ nCurses/panels.c
@@ -3,20 +3,6 @@
 #include <curses.h>
 #include <panel.h>
 
-int curses_init()
-{
-    initscr();
-    cbreak();
-    noecho();
-    return EXIT_SUCCESS;
-}
-
-int curses_quit()
-{
-    endwin();
-    return EXIT_SUCCESS;
-}
-
 int main(int argc, char *argv[])
 {
     WINDOW *w1;
@@ -32,17 +18,22 @@ int main(int argc, char *argv[])
     int begX = 10;
     int ch;
 
-    curses_init();
+    // Curses initialization
+    initscr();
+    cbreak();
+    noecho();
 
     // Init windows
     w1 = newwin(winLines, winColumns, begY, begX);
     w2 = newwin(winLines, winColumns, begY+5, begX);
     w3 = newwin(winLines, winColumns, begY+10, begX);
 
+    // Adding borders to the windows
     box(w1, '|', '-');
     box(w2, '|', '-');
     box(w3, '|', '-');
 
+    // Printing unique strings to each window
     mvwaddstr(w1, 1, 1, "This is window 1");
     mvwaddstr(w2, 1, 1, "This is window 2");
     mvwaddstr(w3, 1, 1, "This is window 3");
@@ -52,13 +43,19 @@ int main(int argc, char *argv[])
     p2 = new_panel(w2);
     p1 = new_panel(w1); // Panel Order: p1 -> p2 -> p3
 
-    set_panel_userptr(p1, p2); // Sets user pointers
+    // Setting panel user pointers
+    // Each panel's user pointer is just a pointer to anything I want.
+    // In this case, it's a pointer to the next window.
+    set_panel_userptr(p1, p2);
     set_panel_userptr(p2, p3);
     set_panel_userptr(p3, p1);
 
     update_panels(); // Updates virtual screen to reflect panel stack.
     doupdate(); // Updates the physical screen (what is actually displayed).
 
+    // Loop for tabbing through panels.
+    // TAB -> Brings the next panel to the top.
+    // q   -> Breaks the loop and exits program.
     top = p1;
     while ((ch = getch()) != 'q')
     {
@@ -73,14 +70,18 @@ int main(int argc, char *argv[])
         doupdate();
     }
 
+    // Freeing memory associated with windows.
     delwin(w1);
     delwin(w2);
     delwin(w3);
 
+    // Freeing memory associated with panels.
     del_panel(p1);
     del_panel(p2);
     del_panel(p3);
-    curses_quit();
+
+    // Exiting curses mode
+    endwin();
 	return EXIT_SUCCESS;	
 }
 
