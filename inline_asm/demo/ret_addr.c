1fec793af79979f9d3d6c38bc86de58e6009fdc9
Curt Bridgers <bridgerspc@gmail.com>
2021-10-19 15:28:28 -0400

    Buffed the macros and added a print state for when foo() returns to main()

--- inline_asm/demo/ret_addr.c
+++ inline_asm/demo/ret_addr.c
@@ -6,52 +6,65 @@
 // This is the same return address printing function
 // as is found is ../inline_asm/ret_addr.c but in macro
 // form.
-#define RET_ADDR() {  \
+
+#define INIT_ADDR(label) \
+    uint64_t *a##label; \
+    uint64_t *b##label; \
     asm volatile ( \
         "mov $1, %%r8;" \
         "lea (%%rbp, %%r8, 8), %0;" \
-        : "=rm" (addr) );}
+        : "=rm" (a##label) );
+
+#define FINAL_ADDR(label)  \
+    asm volatile ( \
+        "mov $1, %%r8;" \
+        "lea (%%rbp, %%r8, 8), %0;" \
+        : "=rm" (b##label) );
+
+
+// Both pointers passed to this function are aliased.
+// This causes initial != final to always return false.
+void checkOF(uint64_t initial, uint64_t final)
+{
+    if (initial != final)
+        printf("Buffer overflow detected\n");
+    else
+        printf("Buffer overflow not detected\n");
+    return;
+}
 
 void target()
 {
-    uint64_t *addr;
-    RET_ADDR();
+    INIT_ADDR(addr);
 
     printf("Target function was called\n");
 
     // Restore the return address to main
-    *addr = 0x555555555226;
+    *aaddr = 0x55555555526d;
+
+    FINAL_ADDR(addr);
+    checkOF(*aaddr, *baddr);
     return;
 }
 
 int foo() {
-    uint64_t *addr; // Tmp variable for the macro
-    uint64_t initial_addr; // tmp variable to hold initial return address
-
-    // Store return address in adr at the beginning of the function
-    RET_ADDR();
-    // Save initial return addres into ret_addr
-    initial_addr = *addr;
+    INIT_ADDR(addr);
 
     int operand1 = 30;
     int operand2 = 25;
 
     // Artificially modifying the return address
-    *addr = 0x555555555149;
+    *aaddr = 0x555555555186;
 
-    // Setting addr to the new, modified return address
-    RET_ADDR();
-    if (initial_addr != *addr)
-        printf("Buffer overflow detected\n");
-    else
-        printf("Buffer overflow not detected\n");
-
+    FINAL_ADDR(addr);
+    checkOF(*aaddr, *baddr);
     return operand1 + operand2;
 }
 
 int main(int argc, char *argv[])
 {
     printf("30 + 25 = %d\n", foo());
+    printf("foo() returned to main()\n");
     return EXIT_SUCCESS;	
 }
 
